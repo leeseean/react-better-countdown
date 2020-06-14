56b3ed2e941eb500484ed73060fe19d73951169f
qiuzhengchen <leeseean@qq.com>
2020-06-15 05:37:02 +0800

    添加类型文件

---
+++ types/index.d.ts
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+interface CountdownProps {
+    count?: number;
+    dayText?: string;
+    callback?: Function;
+    className?: string;
+    style?: React.CSSProperties;
+    rest?: any
+}
+
+interface CountdownState {
+    count?: number;
+}
+
+declare module 'react-better-countdown' {
+    export default class Countdown extends React.Component<CountdownProps, CountdownState> {
+        state: CountdownState;
+        timer: null | number;
+        tick(): void;
+        componentDidMount(): void;
+        componentDidUpdate(prevProps: CountdownProps): void;
+        componentWillUnmount(): void;
+        render: () => React.ReactElement;
+    }
+}
